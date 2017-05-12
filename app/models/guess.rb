201f86eaed52ce7b7c33ec8509f59e5c5a7effb4
Alex Camacho & Carlos Grijalva & Lou Gaeta <pair+alex-e-camacho.cgrijalva90.louisgaeta1@devbootcamp.com>
2017-05-12 11:45:47 -0400

    Began working on model methods

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -1,4 +1,13 @@
 class Guess < ApplicationRecord
   belongs_to :card
   belongs_to :round
+
+  def correct?
+    card.answer == input
+  end
+
+  # first_try = true if correct?
+
+  
+
 end
--- app/models/round.rb
+++ app/models/round.rb
@@ -3,4 +3,13 @@ class Round < ApplicationRecord
   belongs_to :deck
   has_many :guesses
   has_many :cards, through: :guesses
+
+  def complete?
+    deck.length == guesses.select{ |guess| guess.correct? }.length
+  end
+
+  # def first_try_correct
+    # guesses.select{ |guess| }
+  # end
+
 end
