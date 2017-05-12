3e733b9ff8b3d7487ea856f2ad59516cdc8c2396
Alex Camacho <email": null>
2017-05-12 11:38:28 -0400

    Add table columns

--- db/migrate/20170512112544_create_cards.rb
+++ db/migrate/20170512112544_create_cards.rb
@@ -1,4 +1,10 @@
 class CreateCards < ActiveRecord::Migration[5.0]
   def change
+    create_table :cards do |t|
+      t.string     :question, :null => false, :unique => true
+      t.string     :answer, :null => false
+      t.references :deck, :null => false
+
+      t.timestamps(null: false)
   end
 end
--- db/migrate/20170512112549_create_rounds.rb
+++ db/migrate/20170512112549_create_rounds.rb
@@ -1,4 +1,10 @@
 class CreateRounds < ActiveRecord::Migration[5.0]
   def change
+    create_table :rounds do |t|
+      t.references :user, :null => false
+      t.references :deck, :null => false
+
+
+      t.timestamps(null: false)
   end
 end
